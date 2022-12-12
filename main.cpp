569cdfbd326d9009f906450ffc9c88cde6a90e3b
itai953 <itai.sharoni@gmail.com>
2022-12-12 17:13:07 +0200

    changed main to run num_iterations provided by user as argument

--- main.cpp
+++ main.cpp
@@ -3,12 +3,21 @@
 
 int main(int argc, char** argv)
 {
-    Graph result;
-    int g, max_n;
+    Graph result,temp;
+    int g, max_n,num_iterations;
     sscanf(argv[1],"%d",&g);
     sscanf(argv[2],"%d",&max_n);
-    // cout << g << " " << max_n;
+    sscanf(argv[3],"%d",&num_iterations);
     GD(g, max_n, result);
+
+    for(int i = 0; i < num_iterations; i++ )
+    {
+        GD(g, max_n, temp);
+        if (temp.size() < result.size())
+        {
+            result = temp;
+        }
+    }
     cout << result;
     return 0;
 }
