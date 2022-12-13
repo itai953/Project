b5161882e8e94e92c6a669e4a700eee83d0ac34e
Itamar <itamar463mm@gmail.com>
2022-12-13 15:46:03 +0200

    Merge branch 'main' of https://github.com/itai953/Project

---
+++ Data.cpp
@@ -0,0 +1,34 @@
+#include "Data.h"
+
+void calclAverage(vector<vector<int> > &walks, vector<int> &avg, int edges, int g) {
+    for (auto &walk : walks) {
+        for (int i = 0; i < g; ++i) {
+            avg[i] += walk[i];
+        }
+    }
+    for (int &sum: avg) {
+        sum /= edges;
+    }
+}
+
+void writeData(vector<vector<int> > &walks, int edges, int g, const string &filePath) {
+    std::ofstream dataFile(filePath + ".txt");
+    std::vector<int> avg(g - 1);
+    std::vector<int> maxWalk = walks[0];
+    // calculate average for each c(i) of the edges
+    calclAverage(walks, avg, edges, g);
+    // getting max vector by lexicography order
+    for (int i = 1; i < edges; ++i) {
+        if (std::lexicographical_compare(begin(maxWalk), end(maxWalk), begin(walks[i]), end(walks[i]))) {
+            maxWalk = walks[i];
+        }
+    }
+    for (int i = 0; i < g - 1; ++i) {
+        dataFile << avg[i] << " ";
+    }
+    dataFile << "\n";
+    for (int i = 0; i < g - 1; ++i) {
+        dataFile << maxWalk[i] << " ";
+    }
+    dataFile.close();
+}
---
+++ Data.h
@@ -0,0 +1,15 @@
+#ifndef DATA_H
+#define DATA_H
+#include <iostream>
+#include <fstream>
+#include "Algorithms.h"
+using namespace std;
+
+void writeData(vector<vector<int> > &walks, int edges, int g, const string &filePath);
+
+void calclAverage(vector<vector<int> > &walks, vector<int> &avg, int edges, int g);
+
+
+
+
+#endif
